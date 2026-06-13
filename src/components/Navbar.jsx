5605f4b95ad8fa2d6899ff3520589fa5996068f7
Samad Aamir <samadaamir3@gmail.com>
2026-06-13 11:11:32 +0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center w-[80%] m-auto'>
-       <div className="text-2xl text-yellow-300 font-bold ">CineFile</div>
-       <ul className='text-black flex items-center gap-3'>
+    <div className=' bg-[#808080] flex justify-between items-center w-[80%] m-auto rounded p-2 '>
+       <div className="text-2xl text-[#B20710] font-bold ">CineFile</div>
+       <ul className="text-[#B20710] flex items-center gap-3 font-bold">
         <li>Home</li>
         <li>Movies</li>
         <li>Contact</li>
